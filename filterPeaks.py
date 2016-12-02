9554f5f6e6f3f4c68f7359afb75290f4a96bd35e
Marc van Dijk <marcvdijk@gmail.com>
2016-12-02 08:44:15 +0100

    Update program USAGE description

--- filterPeaks.py
+++ filterPeaks.py
@@ -20,33 +20,71 @@ __copyright__ = 'Copyright (c) 2016 Marc van Dijk'
 __rootpath__  = os.path.dirname(__file__)
 
 USAGE = """
-NOEfinder: Making better use of unused peaks (workingtitle)
+NOEfinder: Making better use of unused peaks
 
-Required input files:
+NOEfinder is designed to resolve peaks that have not been used for the structure
+calculation process of a typical protein. These peaks may contain additional
+protein assignments but also protein-ligand NOE peaks. The latter are often not
+a target of structure calculation programs because they focus on resolving the 
+protein structure but they contain valuable information on the orientation of the
+ligand.
+
+Input:
 - peakfile: H-C-H noesy peakfile in Xeasy format
 - Sparky project: a Sparky .proj file
 - PDB: PDB structure file
   
   ./NOEfinder.py -p <peakfile> -y <Sparky project> -s <PDB file>
 
+Output:
+The NOEfinder program reports results as tabular output written to standard out.
+The table reports for each assignment the peak ID and the associated carbon and
+two protons from the w2, w3 and w1 dimensions of the 3D experiment respectively.
+For each dimension the associated residue name and number, atom name and number
+and chemical shift are reported. Each assignment is labeled with a four
+character capitalized string (Cat. column). The first two characters indicate:
+
+- IR: Inter Residue assignment, any C-H-H assignment between two different
+      residues.
+- TB: Through Bond, assignment includes atoms covalently linked (calculated 
+      distance < 1.09 Å)
+- TR: Through Residue, assignment includes atoms part of the same residue 
+      (name and number)
+- NX: No carbon atom found in structure.
+
+The second two characters indicate:
+- PP: Assignment includes protein residues only (standard amino-acids).
+- PO: Assignment includes a protein residue and a non-protein residue judged by
+      residue name.
+- XX: Not assigned, in case of NC. NC=? = NX?
+
+The label allows for easy sorting and filtering of the output table using
+standard UNIX command line tools. The last column of the table holds the
+calculated distances in Å between the carbon atom of the identified 
+carbon-proton pair and the proton in the w1 dimension in case the PDB structure
+file of the associated protein or complex is provided. If such structure file
+is not provided or the distance could not be resolved the distance value is 
+set to 999.000
+
+Settings:
+Basic program settings are defined using the command line interface. 
+Advanced options such as residue and atom renaming conventions (see below) and
+persistent storage of settings are enabled via the python style configuration
+file (config_noefinder.py). Please read the documentation on how to use the
+configuration file in the header of that file.
+
+Residue and atom naming:
+Identification of residues and atoms in structure files requires their naming
+to match those of the assignments which is very often not the case. 
+The NOEfinder program offers advanced residue and atom translation services for
+this purpose. Please consult the config_noefinder.py file for more information
+on how to use it.
+
 Use the -h/--help option for more information on program options
-
-FilterPeaks is designed to resolve peaks that have not been used
-for the structure calculation process of a typical protein.
-These peaks may contain additional protein assignements but also
-protein-ligand NOE peaks. The latter are often not a target of
-structure calculation programs because they focus on resolving
-the protein structure but they contain valuble information on
-the orientation of the ligand.
-
-FilterPeaks performs this task by:
-- Loading unassigned peaks from a 3D NOE experiment (Xeasy format).
-- Find carbond-proton assignment pairs for unassigned peaks
-  matching carbon (13C) chemical shift +/- tolerance and proton (1H)
-  chemical shift +/- tolerance in w2 and w3 dimensions respectivly.
 """
 
-EPILOG = "Version:    {version}, Author:     {author}, License:    {license}".format(author=__author__, version=__version__, license=__license__)
+EPILOG = "Version: {version},   Author: {author},   License: {license}".format(
+    author=__author__, version=__version__, license=__license__)
 
 # Primary program configuration
 STRUCTURE_DICT  = {'id':0, 'atom':None, 'resi':None, 'resn':0, 'coor':(0.00, 0.00, 0.00)}
